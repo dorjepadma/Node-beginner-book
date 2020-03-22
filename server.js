c815c349fa25a106d3975bbb6b320e2ba642faad
dorjepadma <dorjepadma@gmail.com>
2020-03-22 15:23:32 -0700

    index.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let http = require('http');
 
+function start() {
 function onRequest(request, response) {
   console.log('Request received.');
     response.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -8,5 +9,7 @@ function onRequest(request, response) {
 }
 
 http.createServer(onRequest).listen(8888);
-
 console.log('Server has started.');
+}
+
+exports.start = start;
