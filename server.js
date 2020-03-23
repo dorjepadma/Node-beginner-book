dd609b7697de5bbc57ab29fccd1baa289a6784be
dorjepadma <dorjepadma@gmail.com>
2020-03-22 20:32:56 -0700

    uploading .png no problemo

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-const http = require('http');
-const url = require('url'); 
+const http = require("http");
+const url = require("url");
 
 function start(route, handle) {
-function onRequest(request, response) {
-  let postData = '';
-  let pathname = url.parse(request.url).pathname;
-  console.log('Request for' + pathname +  'received.');
+  function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
 
-  request.setEncoding('utf8');
-  request.addListener('data', function(postDataChunk) {
-    postData += postDataChunk;
-    console.log('Recieved Post data chunk' + postDtaChunk + '.');
-  });
-
-  request.addListener('end', function() {
-    route(handle, pathname, response, postData);
-  });
-}
-   http.createServer(onRequest).listen(8888);
-   console.log('Server has started.');
-}
-
-
+ http.createServer(onRequest).listen(8888);
+ console.log("Server has started.");
+ }
 exports.start = start;
