4843bbce07f54740d2b29eea285db4cddd92210d
dorjepadma <dorjepadma@gmail.com>
2020-03-22 17:49:44 -0700

    query data

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const url = require('url');
 
 function start(route, handle) {
 function onRequest(request, response) {
+  let postData = '';
   let pathname = url.parse(request.url).pathname;
   console.log('Request for' + pathname +  'received.');
 
-  route(handle, pathname, response);
+  request.setEncoding('utf8');
+  request.addListener('data', function(postDataChunk) {
+    postData += postDataChunk;
+    console.log('Recieved Post data chunk' + postDtaChunk + '.');
+  });
+
+  request.addListener('end', function() {
+    route(handle, pathname, response, postData);
+  });
 }
    http.createServer(onRequest).listen(8888);
    console.log('Server has started.');
